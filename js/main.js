64c8e37a340b85f3d49d24e2b9ff544ac125f5e9
LiubovBab <lubochka-net@mail.ru>
2021-06-01 15:11:24 +0300

    Выполнение задания 2.14

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//доп. функция "Количество машин"
+function car (min, max){
+  const quantity = Math.random();
+  return Math.floor(quantity);
+}
+//Функция для проверки максимальной длины строки
+const lengthComments = 141;
+function writeComments (comments, lengthComments){
+  if (comments<=lengthComments){
+    writeComments=true;
+  }
+  writeComments=false;
+}
